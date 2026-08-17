fb7a0002796428bdb3da45fccdfbb4a0e538c144
Shalom Digital Solutions <nounonvenanromain@gmail.com>
2026-08-17 10:24:49 +0100

    feat(backend): link organizations to users

--- backend/app/Models/Organization.php
+++ backend/app/Models/Organization.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Organization extends Model
@@ -16,5 +17,12 @@ class Organization extends Model
     public function transactions(): HasMany
     {
         return $this->hasMany(FinancialTransaction::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)
+            ->withPivot('role_id')
+            ->withTimestamps();
     }
 }

efdc0d24d6234b63d242e1daa7baec8b8a03b959
Shalom Digital Solutions <nounonvenanromain@gmail.com>
2026-08-19 20:33:49 +0100

    chore(backend): add cache configuration

---
+++ backend/config/cache.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'default' => env('CACHE_STORE', 'file'),
+    'stores' => [
+        'array' => ['driver' => 'array', 'serialize' => false],
+        'file' => ['driver' => 'file', 'path' => storage_path('framework/cache/data')],
+    ],
+    'prefix' => env('CACHE_PREFIX', 'finance_pro'),
+];
